82a503dd940267be07e7bcb86b78a989c49c9fb7
Alexandro Millan Preciado <85408352+Alexandro1704@users.noreply.github.com>
2022-04-19 19:42:32 -0500

    Create Tipo_restaurante.java

---
+++ demosafetyfood/src/main/java/pe/edu/upc/entities/Tipo_restaurante.java
@@ -0,0 +1,55 @@
+package pe.edu.upc.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Tipo_restaurante")
+public class Tipo_restaurante {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int idRestaurante;
+	
+	@Column(name = "Nombredepartamento", nullable = false,length = 47)
+	private String nombreRestaurante;
+	
+	
+	public Tipo_restaurante() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Tipo_restaurante(int idRestaurante, String nombreRestaurante) {
+		super();
+		this.idRestaurante = idRestaurante;
+		this.nombreRestaurante = nombreRestaurante;
+		
+	}
+
+	public int getIdRestaurante() {
+		return idRestaurante;
+	}
+
+	public void setIdRestaurante(int idDepartamento) {
+		this.idRestaurante = idRestaurante;
+	}
+
+	public String getNombreRestaurante() {
+		return nombredepartamento;
+	}
+
+	public void setNombreRestaurante(String nombredepartamento) {
+		this.nombreRestaurante = nombreRestaurante;
+	}
+
+	
+	
+	
+	
+
+}
